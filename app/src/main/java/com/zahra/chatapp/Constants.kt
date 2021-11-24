0589d0306da157afe5e09e7d89873ce8296a4da2
Fathimah Az Zahra <azahrx@gmail.com>
2021-11-25 00:35:13 +0700

    implement push notifications

---
+++ app/src/main/java/com/zahra/chatapp/Constants.kt
@@ -0,0 +1,10 @@
+package com.zahra.chatapp
+
+class Constants {
+
+    companion object {
+        const val BASE_URL = "https://fcm.googleapis.com"
+        const val SERVER_KEY = "AAAA_NHvW7Y:APA91bExrE7oe7HAJ07ouME_KJ5y6dif3wodI8E0CQOM9JE9wzycarNLDXV8cVv2L4D0dmYot0OQHZoVOwHxTVHB7sLq9icHfKokU8-y4Lgp-TEyuHNO_SH8WSRzv8NKxxxY_tMNPDyl"
+        const val CONTENT_TYPE = "application/json"
+    }
+}
--- app/src/main/java/com/zahra/chatapp/MainActivity.kt
+++ app/src/main/java/com/zahra/chatapp/MainActivity.kt
@@ -1,21 +1,59 @@
 package com.zahra.chatapp
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.util.Log
+import android.content.Context
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import com.zahra.chatapp.databinding.ActivityMainBinding
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.gson.Gson
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+const val TOPIC = "/topics/myTopic"
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        FirebaseMessaging.getInstance().subscribeToTopic(TOPIC)
+
+        val btnSend = findViewById<Button>(R.id.btnSend)
+        val etTitle = findViewById<EditText>(R.id.etTitle)
+        val etMessage = findViewById<EditText>(R.id.etMessage)
+//        val etToken = findViewById<EditText>(R.id.etToken)
+
+        btnSend.setOnClickListener {
+            val title = etTitle.text.toString()
+            val message = etMessage.text.toString()
+            if (title.isNotEmpty() && message.isNotEmpty()) {
+                PushNotification(
+                    NotificationData(title, message),
+                    TOPIC
+                ).also {
+                    sendNotification(it)
+                }
+            }
+        }
+    }
+
+    private fun sendNotification(notification: PushNotification) = CoroutineScope(Dispatchers.IO).launch {
+        try {
+            val response = RetrofitInstance.api.postNotification(notification)
+            if(response.isSuccessful) {
+                Log.d(TAG, "Response: ${Gson().toJson(response)}")
+            } else {
+                Log.e(TAG, response.errorBody().toString())
+            }
+        } catch(e: Exception) {
+            Log.e(TAG, e.toString())
+        }
     }
 }
